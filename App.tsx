21c649b428b3d29e4523315c4a9b6312348d3a2c
lifeguidancewithjesper <sorensenjb05@yahoo.com>
2025-09-16 20:47:54 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { registerRootComponent } from 'expo';
+import App from './App';
 
-export default function App() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
-      <StatusBar style="dark" />
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Pound Drop</Text>
-      <Text style={{ fontSize: 16, marginTop: 10 }}>Build Success Test</Text>
-    </View>
-  );
-}
+registerRootComponent(App);
